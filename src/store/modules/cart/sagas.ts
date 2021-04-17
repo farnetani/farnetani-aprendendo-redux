c97e74f5963374927bb25413cba23f783afcff0f
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-04-17 17:07:12 -0300

    dividido as actions em 3 pra tratar o middlweare

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,8 +1,22 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, takeLatest, select } from 'redux-saga/effects'
+import { IState } from '../..'
+import { addProductCartRequest } from './actions'
 
-function checkProductStock() {
-  console.log('Adicionou ao carrinho')
+type CheckProductStockRequest = ReturnType<typeof addProductCartRequest>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    ) // ?? = ou retorna 0, seria o mesmo || 0
+  })
+  console.log(currentQuantity)
 }
 
 // no primeiro parametro devemos passar a nossa action e no segundo parametro o que será executado
-export default all([takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)])
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock)
+])
