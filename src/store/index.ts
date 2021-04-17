b27028852ef235413b1cd5fc88a2cac9c60be6ad
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-04-17 15:05:37 -0300

    antes de abstrair alguns componentes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import { createStore } from 'redux'
 import { ICartState } from './modules/cart/types'
 import rootReducer from './modules/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 // Tem o formato do nosso State Global = ao tipo: ICartState =  items: ICartItem[]
 export interface IState {
   cart: ICartState
 }
-
-const store = createStore(rootReducer)
+/* eslint-disable no-underscore-dangle */
+const store = createStore(rootReducer, composeWithDevTools()) //composeWithDevTools() é para ativar o Devtools do redux
 
 export default store
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -12,28 +12,40 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   //   items: []
   // }
 
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload
 
-      // Usando a lib immer fica mais fácil de entendermos
-      return produce(state, (draft) => {
-        draft.items.push({
-          product,
-          quantity: 1
-        })
-      })
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        )
 
-      // Retorna um novo elemento de state, sobrescreve todos os items, e adicionamos um novo item com o product e a qtde = 1
-      // return {
-      //   ...state,
-      //   items: [...state.items, { product, quantity: 1 }] // Aqui depois da virgula significa que estamos adicionando o novo elemento
-      // }
+        // -1 = não encontrado
+        if (productInCartIndex >= 0) {
+          // adiciono a quantidade
+          draft.items[productInCartIndex].quantity++
+        } else {
+          // Usando a lib immer fica mais fácil de entendermos
+          draft.items.push({
+            product,
+            quantity: 1
+          })
+        }
+
+        break //porque eu nao tenho o return, jogamos ele pra fora do switch
+
+        // Retorna um novo elemento de state, sobrescreve todos os items, e adicionamos um novo item com o product e a qtde = 1
+        // return {
+        //   ...state,
+        //   items: [...state.items, { product, quantity: 1 }] // Aqui depois da virgula significa que estamos adicionando o novo elemento
+        // }
+      }
+      default: {
+        return draft
+      }
     }
-    default: {
-      return state
-    }
-  }
+  })
 }
 
 export default cart
